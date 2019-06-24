4be77190203526c7e11abd5362fc7e9b274fe331
suzuki-navi <suzuki.navi.dev@gmail.com>
2019-06-24 23:18:07 +0900

    add support graalvm

--- anylang.sh
+++ anylang.sh
@@ -19,13 +19,18 @@ Options:
     --prefx      インストールするディレクトリを指定 (デフォルトは $HOME/.xsv-anylang)
     --jdk=<VER>  インストールするopenjdkのバージョンを指定
     --sbt=<VER>  インストールするsbtのバージョンを指定。 --jdk も併せて指定が必要
+    --graalvm=<VER>  インストールするGraalVMのバージョンを指定
 
-jdk
+JDK
 ex) --jdk=11
 
 sbt
 https://www.scala-sbt.org/download.html
 ex) --sbt=1.2.8
+
+GraalVM
+https://github.com/oracle/graal/releases
+ex) --graalvm=19.0.2
 EOF
   exit 1
 }
@@ -41,6 +46,9 @@ while [ "$#" != 0 ]; do
         --sbt=* )
             SBT_VERSION="${1#*=}"
             ;;
+        --graalvm=* )
+            GRAALVM_VERSION="${1#*=}"
+            ;;
         --* )
             echo "Option \`${1}\` is not supported." >&1
             exit 1
@@ -54,16 +62,21 @@ done
 : "${PREFIX:=$HOME/.xsv-anylang}"
 
 ################################################################################
-# jdk
+# JDK
 ################################################################################
 
 install_jdk() {
+    if [ -v GRAALVM_VERSION ]; then
+        echo "cannot use both --jdk=* and --graalvm=* options" >&2
+        exit 1
+    fi
+
     if [ ! -x "$PREFIX/jdk-${JDK_VERSION}/bin/java" ]; then
         uname=$(uname)
         if [ "$uname" = "Darwin" ]; then
-            openjdk_os_name='osx'
+            os_name='osx'
         elif [ "$uname" = "Linux" ]; then
-            openjdk_os_name='linux'
+            os_name='linux'
         else
             echo "Unknown OS: $uname" >&2
             exit 1
@@ -71,7 +84,7 @@ install_jdk() {
 
         tmppath="$PREFIX/jdk-${JDK_VERSION}-$$"
         mkdir -p $tmppath
-        url="https://download.java.net/java/ga/jdk${JDK_VERSION}/openjdk-${JDK_VERSION}_${openjdk_os_name}-x64_bin.tar.gz"
+        url="https://download.java.net/java/ga/jdk${JDK_VERSION}/openjdk-${JDK_VERSION}_${os_name}-x64_bin.tar.gz"
 
         echo "cd $tmppath; curl -L $url | tar xzf -"
         (
@@ -82,7 +95,7 @@ install_jdk() {
             if [ -e "$PREFIX/jdk-${JDK_VERSION}" ]; then
                 rm -rf "$PREFIX/jdk-${JDK_VERSION}"
             fi
-            if [ $openjdk_os_name = "osx" ]; then
+            if [ $os_name = "osx" ]; then
                 echo mv $tmppath/jdk-$JDK_VERSION.jdk "$PREFIX/jdk-${JDK_VERSION}"
                 mv $tmppath/jdk-$JDK_VERSION.jdk "$PREFIX/jdk-${JDK_VERSION}"
             else
@@ -129,11 +142,55 @@ install_sbt() {
 }
 
 ################################################################################
+# GraalVM
+################################################################################
+
+install_graalvm() {
+    if [ -v JDK_VERSION ]; then
+        echo "cannot use both --jdk=* and --graalvm=* options" >&2
+        exit 1
+    fi
+
+    if [ ! -x "$PREFIX/graalvm-${GRAALVM_VERSION}/bin/java" ]; then
+        uname=$(uname)
+        if [ "$uname" = "Darwin" ]; then
+            os_name='darwin'
+        elif [ "$uname" = "Linux" ]; then
+            os_name='linux'
+        else
+            echo "Unknown OS: $uname" >&2
+            exit 1
+        fi
+
+        tmppath="$PREFIX/graalvm-${GRAALVM_VERSION}-$$"
+        mkdir -p $tmppath
+        url="https://github.com/oracle/graal/releases/download/vm-${GRAALVM_VERSION}/graalvm-ce-${os_name}-amd64-${GRAALVM_VERSION}.tar.gz"
+        echo "cd $tmppath; curl -L $url | tar xzf -"
+        (
+            cd $tmppath
+            curl -L $url | tar xzf -
+            $tmppath/graalvm-ce-${GRAALVM_VERSION}/bin/gu install native-image
+        )
+        if [ ! -e "$PREFIX/graalvm-${GRAALVM_VERSION}/bin/java" ]; then
+            if [ -e "$PREFIX/graalvm-${GRAALVM_VERSION}" ]; then
+                rm -rf "$PREFIX/graalvm-${GRAALVM_VERSION}"
+            fi
+
+            mv $tmppath/graalvm-ce-${GRAALVM_VERSION} "$PREFIX/graalvm-${GRAALVM_VERSION}"
+        fi
+        rm -rf $tmppath
+    fi
+
+    export PATH="$PREFIX/graalvm-${GRAALVM_VERSION}/bin:$PATH"
+}
+
+################################################################################
 # main
 ################################################################################
 
 [ -v JDK_VERSION ] && install_jdk
 [ -v SBT_VERSION ] && install_sbt
+[ -v GRAALVM_VERSION ] && install_graalvm
 
 [ "$#" = 0 ] && usage
 
